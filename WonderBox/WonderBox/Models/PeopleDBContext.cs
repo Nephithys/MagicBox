fd30fb59c7d87323123f77d04ee389d1a66339b6
Nephithys <Nephithys@gmail.com>
2016-02-24 08:57:13 -0600

    Created and migrated Profile data base as well as created the profile model and controler

---
+++ WonderBox/WonderBox/Models/PeopleDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WonderBox.Models
+{
+    public class PeopleDBContext:DbContext
+    {
+        public PeopleDBContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<People> LotsOfPeople { get; set; }
+    }
+}
